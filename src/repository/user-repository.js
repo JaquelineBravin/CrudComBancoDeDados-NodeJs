4de7196be2043e19ab49e117eb26e388395ce2db
JaquelineBravin <jaqbravin@hotmail.com>
2026-04-01 22:56:41 -0400

    error handling: create/delete user

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -16,6 +16,12 @@ class userRepository {
   }
 
   async createUser(User) {
+    const existingUser = await this.getUserByName(User.nameUser);
+    if (existingUser) {
+      console.log(existingUser);
+      throw new Error('User already exists');
+    }
+
     const sql = `INSERT INTO users (nameUser, passwordUser) VALUES (?, ?)`;
     const values = [User.nameUser, User.passwordUser];
     return new Promise((resolve, reject) => {
@@ -32,6 +38,7 @@ class userRepository {
   async updateUser(id, user) {
     const sql = `UPDATE users SET nameUser = ?, passwordUser = ? WHERE id = ?`;
     const values = [user.nameUser, user.passwordUser, id];
+
     return new Promise((resolve, reject) => {
       connection.query(sql, values, (err, result) => {
         if (err) {
@@ -44,8 +51,29 @@ class userRepository {
   }
 
   async deleteUser(id) {
-    const sql = `DELETE FROM users WHERE id = ?`;
-    const values = [id];
+    const existingUser = await this.getUserById(id);
+    if (existingUser && existingUser.length > 0) {
+      const sql = `DELETE FROM users WHERE id = ?`;
+      const values = [id];
+
+      return new Promise((resolve, reject) => {
+        connection.query(sql, values, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+    } else {
+      throw new Error('User not found');
+    }
+  }
+
+  async getUserByName(nameUser) {
+    const sql = `SELECT * FROM users WHERE nameUser = ?`;
+    const values = [nameUser];
+
     return new Promise((resolve, reject) => {
       connection.query(sql, values, (err, result) => {
         if (err) {
@@ -57,9 +85,12 @@ class userRepository {
     });
   }
 
-  async getUserByName(nameUser) {
-    const sql = `SELECT * FROM users WHERE nameUser = ?`;
-    const values = [nameUser];
+  async getUserById(id) {
+    const sql = `SELECT * FROM users WHERE id = ?`;
+    const values = [id];
+    if (!id) {
+      throw new Error('ID is required');
+    }
     return new Promise((resolve, reject) => {
       connection.query(sql, values, (err, result) => {
         if (err) {
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserRepository from '../repository/user-repository.js';
+import { StatusCodes } from 'http-status-codes';
 
 const userRoutes = Router();
 
@@ -8,40 +9,64 @@ userRoutes.post('/user', createUser);
 userRoutes.put('/user/:id', updateUser);
 userRoutes.delete('/user/:id', deleteUser);
 userRoutes.get('/user/:nameUser', getUserByName);
+userRoutes.get('/user/:id', getUserById);
 
 async function getAllUser(req, res) {
   const users = await UserRepository.getAllUsers();
-  res.json(users);
+  res.status(StatusCodes.OK).json(users);
 }
 
 async function createUser(req, res) {
   try {
     const user = req.body;
-    const createdUser = await UserRepository.createUser(user);
-    res.status(201).json({ message: 'Usuário criado com sucesso' });
-  } catch (error) {
+    await UserRepository.createUser(user);
     res
-      .status(500)
-      .json({ message: 'Erro ao criar usuário', error: error.message });
+      .status(StatusCodes.CREATED)
+      .json({ message: 'User created successfully' });
+  } catch (error) {
+    const message = error.message;
+    let status = StatusCodes.INTERNAL_SERVER_ERROR;
+
+    if (message.includes('required')) {
+      status = StatusCodes.BAD_REQUEST;
+    } else if (message.includes('already exists')) {
+      status = StatusCodes.CONFLICT;
+    }
+
+    res.status(status).json({ error: message });
   }
 }
 
 async function updateUser(req, res) {
-  const id = req.params.id;
-  const user = req.body;
-  const updatedUser = await UserRepository.updateUser(id, user);
-  res.json(updatedUser);
+  try {
+    const id = req.params.id;
+    const user = req.body;
+    const updatedUser = await UserRepository.updateUser(id, user);
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(error);
+  }
 }
 
 async function deleteUser(req, res) {
-  const id = req.params.id;
-  const deletedUser = await UserRepository.deleteUser(id);
-  res.json(deletedUser);
+  try {
+    const id = req.params.id;
+    const deletedUser = await UserRepository.deleteUser(id);
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(StatusCodes.NOT_FOUND).json({ error: error.message });
+  }
 }
 
 async function getUserByName(req, res) {
   const name = req.params.nameUser;
   const user = await UserRepository.getUserByName(name);
+  res.json(user);
+}
+
+async function getUserById(req, res) {
+  const id = req.params.id;
+  const user = await UserRepository.getUserById(id);
   res.json(user);
 }
 
